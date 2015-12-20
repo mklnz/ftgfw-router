d090f9caf50d74fec143d0d437922b968f6e8e5a
Michael Shi <michael@shi.co.nz>
2015-12-21 00:01:58 +0800

    Updated chmod, fixed install script

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 # Partition usb drive
+echo "LABEL=ENTWARE /opt ext3 rw,noatime 1 1" >> /etc/fstab
 umount /dev/sda?
 echo -e "o\nn\np\n1\n\n+500M\nw" | fdisk /dev/sda
 umount /dev/sda?
 mkfs.ext3 -L ENTWARE /dev/sda1
-
-echo "LABEL=ENTWARE /opt ext3 rw,noatime 1 1" >> /etc/fstab
+umount /dev/sda?
+mount /opt
 
 # Install entware
-mount /opt
 entware-install.sh
 
 # Install packages
@@ -18,7 +18,7 @@ opkg install bash vim openssh-sftp-server ruby ruby-enc-extra \
 ruby-net ruby-openssl ruby-optparse ruby-yaml git git-http ca-certificates shadowsocks-libev
 
 # Config
-git clone https://github.com/mklnz/ftgfw-router.git /opt/ftgfw-router
+git clone --recursive https://github.com/mklnz/ftgfw-router.git /opt/ftgfw-router
 
 nvram set script_usbmount='/opt/ftgfw-router/usb_mount.sh'
 nvram set script_usbumount='/opt/ftgfw-router/usb_unmount.sh'
@@ -26,7 +26,7 @@ nvram set script_init='echo "LABEL=ENTWARE /opt ext3 rw,noatime 1 1" >> /etc/fst
 nvram set script_fire='/opt/ftgfw-router/firewall.sh'
 nvram set script_wanup='/opt/ftgfw-router/wan_up.sh'
 nvram set dnsmasq_custom='strict-order
-conf-dir=/opt/ftgfw-router/gfwlist2dnsmasq/dnsmasq.d'
+conf-dir=/opt/ftgfw-router/gfwlist2dnsmasq-rb/dnsmasq.d'
 
 nvram commit
 
